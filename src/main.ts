d3f34215c3f046d493669bb5bb49ae3b688dc896
Camden Mecklem <c@camdenmecklem.com>
2021-10-15 17:49:51 -0400

    remove konva dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueKonva from 'vue-konva'
 import { store, key } from './store'
 
 const app = createApp(App)
-app.use(VueKonva)
 app.use(store, key)
 app.mount('#app')
