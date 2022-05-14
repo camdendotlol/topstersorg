fa43205e59566328f93bd497ade5deb1111a472b
Camden Mecklem <c@camdenmecklem.com>
2022-05-13 21:16:38 -0400

    remember custom search type on refresh

--- src/components/Sidebar/SearchBox/TypeDropdown.vue
+++ src/components/Sidebar/SearchBox/TypeDropdown.vue
@@ -56,7 +56,7 @@ export default defineComponent({
   },
   mounted () {
     const storedType = localStorage.getItem('activeTab')
-    if (storedType && ['music', 'books', 'games', 'movies', 'tv'].includes(storedType)) {
+    if (storedType && ['music', 'books', 'games', 'movies', 'tv', 'custom'].includes(storedType)) {
       this.$emit('setSearchType', storedType)
     }
   },
