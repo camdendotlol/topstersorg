172e2d3178b214240df51a15306fdc8877bcc9ef
Camden Mecklem <c@camdenmecklem.com>
2023-01-16 18:50:51 -0500

    take down RIP notice

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,22 +1,13 @@
 <script setup lang="ts">
-// const subtitles = [
-//   'over 9000 charts served',
-//   'yet another chart site'
-// ]
+const subtitles = [
+  'over 9000 charts served',
+  'yet another chart site'
+]
 </script>
 
 <template>
   <h1>topsters.org</h1>
-  <!-- <h3>{{ subtitles[Math.floor(Math.random() * subtitles.length)] }}</h3> -->
-  <h3>
-    <a
-      href="https://www.instagram.com/p/CnaYUwKrISh/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      RIP Yukihiro Takahashi :(
-    </a>
-  </h3>
+  <h3>{{ subtitles[Math.floor(Math.random() * subtitles.length)] }}</h3>
 </template>
 
 <style scoped>
