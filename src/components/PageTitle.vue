41f52fa091248f47f9e02fb3d183fc950d9b4a33
Camden Mecklem <c@camdenmecklem.com>
2023-06-03 16:45:48 -0400

    take down upgrade note

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -1,14 +1,13 @@
 <script setup lang="ts">
-// const subtitles = [
-//   'over 9000 charts served',
-//   'yet another chart site'
-// ]
+const subtitles = [
+  'over 9000 charts served',
+  'yet another chart site'
+]
 </script>
 
 <template>
   <h1>topsters.org</h1>
-  <!-- <h3>{{ subtitles[Math.floor(Math.random() * subtitles.length)] }}</h3> -->
-  <h4>Announcement: I'm updating the backend so there might be some occasional search downtime over the coming 30-60 minutes.</h4>
+  <h3>{{ subtitles[Math.floor(Math.random() * subtitles.length)] }}</h3>
 </template>
 
 <style scoped>
