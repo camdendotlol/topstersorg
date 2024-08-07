ce4d5ef257ad4ce157ca5379d848c77f9628f000
Camden Mecklem <c@camdenmecklem.com>
2024-08-06 20:07:18 -0400

    switch to Vite replacement strings

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePluginRadar({
-    analytics: {
-      id: 'G-4GL8BY0YV1'
-    }
-  })]
+  plugins: [vue()]
 })
