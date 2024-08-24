30c3d54fe6e7ceb2511648c0922c9a92f2011495
Camden Mecklem <c@camdenmecklem.com>
2024-08-24 13:04:21 -0400

    hmmmm maybe don't say that

--- src/components/Sidebar/Info.vue
+++ src/components/Sidebar/Info.vue
@@ -47,7 +47,7 @@ const changes: ChangelogItem[] = [
     <h3>Why can't I find any King Crimson albums?</h3>
     <p>Not everything in the databases has cover art attached. Because charts require cover art, Topsters 3 filters out items that are missing it. For example, occasionally a band decides they don't want their cover art to appear in databases anymore, so they file a copyright takedown. You may have seen this happen on other sites such as Rate Your Music. This is very rare but it can affect popular items--as of this writing, all of King Crimson's releases are missing album art on Last.fm and as a result will not appear in search results.</p>
     <h3>Why is it called Topsters 3?</h3>
-    <p>The topster concept has been around for a while. Topsters 1 shut down years ago and Topsters 2 doesn't support games, books, or TV shows. That's why I made a new one. All three sites were made by different people. If you want to create Topsters 4, go for it!</p>
+    <p>The topster concept has been around for a while. Topsters 1 shut down years ago and Topsters 2 doesn't support games, books, or TV shows. That's why I made a new one.</p>
     <h3>Why is book searching slow, why does it sometimes not work, why is it missing a book, etc.?</h3>
     <p>I'm using OpenLibrary, which is affiliated with the Internet Archive and runs on donations. The music and games APIs are both from companies that can afford top-tier reliability. But OpenLibrary is a bit less reliable. Don't judge them too much for it, and maybe <a href="https://archive.org/donate/?platform=ol&origin=olwww-TopNavDonateButton">donate to them</a> to help them improve their services.</p>
   </div>
