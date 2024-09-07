09455afbea165c301598440d4714f944f63f80e5
Camden Mecklem <c@camdenmecklem.com>
2024-09-07 17:25:11 -0400

    remove ancient redirect code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { redirectUsers } from './helpers/redirect'
 import Home from './Home.vue'
 import './global.css'
-
-onMounted(async () => {
-  // Redirect new users to topsters.org
-  if (window.location.href.includes('ostrakon.xyz')) {
-    redirectUsers()
-  }
-})
 </script>
 
 <template>
